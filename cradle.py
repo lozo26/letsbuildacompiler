77b4b3813511678ad768741872d7022246c0b28e
Lorenzo Pena <lozo26@gmail.com>
2013-04-04 23:23:34 -0600

    Fix indentation white space (replace tabs w/ spaces only)
    This is not a huge issue but my editor is configured to use spaces
    by default and I often forget to change it so making everything
    match my default is easier for me.

--- cradle.py
+++ cradle.py
@@ -5,73 +5,73 @@ look = '' # Lookahead character
 
 # Read New Character From Input Stream
 def GetChar():
-	global look
-	look = sys.stdin.read(1)
+    global look
+    look = sys.stdin.read(1)
 
 # Report an Error
 def Error(s):
-	print ''
-	print 'Error: ' + s + '.'
+    print ''
+    print 'Error: ' + s + '.'
 
 # Report Error and Halt
 def Abort(s):
-	Error(s)
-	sys.exit()
+    Error(s)
+    sys.exit()
 
 # Report What Was Expected
 def Expected(s):
-	Abort(s + ' Expected')
- 
+    Abort(s + ' Expected')
+
 # Match a Specific Input Character
 def Match(x):
-	global look
-	if look == x:
-		GetChar()
-	else:
-		Expected(''' + x + ''')
+    global look
+    if look == x:
+        GetChar()
+    else:
+        Expected(''' + x + ''')
 
 # Recognize an Alpha Character
 def IsAlpha(c):
-	return c.isalpha()
+    return c.isalpha()
 
 # Recognize a Decimal Digit
 def IsDigit(c):
-	return c.isdigit()
+    return c.isdigit()
 
 # Get an Identifier
 def GetName():
-	val = 0
-	global look
-	if not IsAlpha(look):
-		Expected('Name')
-	else:
-		val = look.upper()
-		GetChar()
-		return val #? maybe not intented?
+    val = 0
+    global look
+    if not IsAlpha(look):
+        Expected('Name')
+    else:
+        val = look.upper()
+        GetChar()
+        return val #? maybe not intented?
 
 # Get a Number
 def GetNum():
-	val = 0
-	global look
-	if not IsDigit(look):
-		Expected('Integer')
-	else:
-		val = look.upper()
-		GetChar()
-		return val #? maybe not intented?
+    val = 0
+    global look
+    if not IsDigit(look):
+        Expected('Integer')
+    else:
+        val = look.upper()
+        GetChar()
+        return val #? maybe not intented?
 
 # Output a String with Tab
 def Emit(s):
-	print '\t' , s
+    print '\t' , s
 
 # Output a String with Tab and CRLF
 def EmitLn(s):
-	Emit(s)
-	print '\n'
+    Emit(s)
+    print '\n'
 
 # Initialize 
 def Init():
-	GetChar()
+    GetChar()
 
 #def main():
 #	Init()
